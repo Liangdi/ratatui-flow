48d5bd025d98c8544ebf0930bebfc96a34b6a069
jaxter184 <jaxter184@gmail.com>
2024-09-05 20:17:03 -0500

    Add line style to example

--- examples/tiny.rs
+++ examples/tiny.rs
@@ -2,7 +2,8 @@
 
 use ratatui::{
 	backend::CrosstermBackend,
-	Frame, Terminal, widgets::{Paragraph, BorderType}, layout::Rect
+	Frame, Terminal, widgets::{Paragraph, BorderType}, layout::Rect,
+	style::{Style, Color}
 };
 
 use tui_nodes::*;
@@ -49,12 +50,12 @@ fn ui(f: &mut Frame, _app: &App) {
 		],
 		vec![
 			Connection::new(0,0,1,0).with_line_type(LineType::Double), // a | b
-			Connection::new(1,0,2,0).with_line_type(LineType::Thick), // b | c
+			Connection::new(1,0,2,0).with_line_type(LineType::Thick).with_line_style(Style::default().fg(Color::Red)), // b | c
 			Connection::new(3,0,2,1).with_line_type(LineType::Double), // d > c
 			Connection::new(4,0,3,0), // e | d
 			Connection::new(4,0,0,1), // e | d
 			Connection::new(5,0,1,1), // f > b
-			Connection::new(5,0,4,1), // f > e
+			Connection::new(5,0,4,1).with_line_type(LineType::Thick).with_line_style(Style::default().fg(Color::Red)), // f > e
 			Connection::new(6,0,0,0), // g | a
 			Connection::new(6,0,5,0).with_line_type(LineType::Double), // g | f
 		],
